b231cbfb0a540d2ed65546cc27503b1a46b357e9
Manuel Duarte <carlosd99m@gmail.com>
2021-11-03 23:55:49 -0600

    feat: post about sqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_flutter/model/user.dart';
+import 'package:sqlite_flutter/services/database_handler.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key : key);
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter SQL'),
     );
   }
 }
@@ -28,11 +31,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late DatabaseHandler handler;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    this.handler = DatabaseHandler();
+    this.handler.initializeDB().whenComplete(() async {
+      await this.addUsers();
+      setState(() {});
     });
   }
 
@@ -42,25 +49,60 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title!),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: FutureBuilder(
+        future: this.handler.retrieveUsers(),
+        builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Icon(Icons.delete_forever),
+                  ),
+                  key: ValueKey<int>(snapshot.data![index].id!),
+                  onDismissed: (DismissDirection direction) async {
+                    await this.handler.deleteUser(snapshot.data![index].id!);
+                    setState(() {
+                      snapshot.data!.remove(snapshot.data![index]);
+                    });
+                  },
+                  child: Card(
+                      child: ListTile(
+                    contentPadding: EdgeInsets.all(8.0),
+                    title: Text(snapshot.data![index].name),
+                    subtitle: Text(snapshot.data![index].age.toString()),
+                  )),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
+  }
+
+  Future<int> addUsers() async {
+    User firstUser = User(name: "peter", age: 24, country: "Lebanon");
+    User secondUser = User(name: "Messi", age: 31, country: "United Kingdom");
+    User thirdUser = User(name: "john", age: 31, country: "United Kingdom");
+    User forUser = User(name: "CR7", age: 31, country: "United Kingdom");
+    User fiveUser = User(name: "Neymar", age: 31, country: "United Kingdom");
+    User sixUser = User(name: "Verrati", age: 31, country: "United Kingdom");
+    User sevenUser = User(name: "Cavani", age: 31, country: "United Kingdom");
+    User eightUser = User(name: "Miguel", age: 31, country: "United Kingdom");
+    User nineUser = User(name: "Juan", age: 31, country: "United Kingdom");
+    User tenUser = User(name: "Pedro", age: 31, country: "United Kingdom");
+    User elevenUser = User(name: "Fati", age: 31, country: "United Kingdom");
+    List<User> listOfUsers = [firstUser, secondUser];
+    return await this.handler.insertUser(listOfUsers);
   }
 }
---
+++ lib/services/database_handler.dart
@@ -0,0 +1,47 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqlite_flutter/model/user.dart';
+
+class DatabaseHandler {
+  Future<Database> initializeDB() async {
+    String path = await getDatabasesPath();
+    return openDatabase(
+      join(path, 'example.db'),
+      onCreate: (database, version) async {
+        await database.execute(
+          "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,age INTEGER NOT NULL, country TEXT NOT NULL, email TEXT)",
+        );
+      },
+      version: 1,
+    );
+  }
+
+  //Saving Data in SQLite
+
+  Future<int> insertUser(List<User> users) async {
+    int result = 0;
+    final Database db = await initializeDB();
+    for (var user in users) {
+      result = await db.insert('users', user.toMap());
+    }
+    return result;
+  }
+
+  //Reading Data from SQLite
+
+  Future<List<User>> retrieveUsers() async {
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResult = await db.query('users');
+    return queryResult.map((e) => User.fromMap(e)).toList();
+  }
+
+  // Delete Data From SQLite
+  Future<void> deleteUser(int id) async {
+    final db = await initializeDB();
+    await db.delete(
+      'users',
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+}
